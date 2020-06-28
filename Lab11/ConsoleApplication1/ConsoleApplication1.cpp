db37aa87b372ceeb203ff111b9f78e39e3e4076e
a-ha25 <56535178+a-ha25@users.noreply.github.com>
2020-06-28 16:04:51 +0300

    Add files via upload

---
+++ Lab11/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,178 @@
+﻿#include <iostream>
+#include <limits>
+#include <conio.h>
+using namespace std;
+
+struct stack {
+	int inf;
+	stack* next;
+};
+
+void input(int& a)
+{
+	while (true)
+	{
+		cin >> a;
+		if (cin.good()) {
+			break;
+		}
+		cout<< "Wrong input" <<endl;
+	}
+}
+stack* addElements(stack* begin)
+{
+	int inf;
+	input(inf);
+	stack* t = new stack;
+	t->inf = inf;
+	t->next = begin;
+	return t;
+}
+void showStack(stack* begin)
+{
+	cout << "--------------Stack--------------" <<endl;
+	stack* p = begin;
+	while (p != NULL) {
+		cout << "\t\t" << p->inf << endl;
+		p = p->next;
+	}
+	cout << endl;
+}
+void deleteStack(stack** begin)
+{
+	while ((*begin) != NULL) {
+		stack* t = *begin;
+		(*begin) = (*begin)->next;
+		delete t;
+	}
+	cout << "Stack cleared" <<endl;
+}
+
+
+void deleteItems(stack* begin) {
+	stack* curr = begin;
+	while (curr->next != NULL)
+	{
+		stack* tmp = curr->next; // удаляемый элемент
+		if (tmp->next)
+		{
+			curr->next = tmp->next;
+			delete tmp;
+
+		}
+		/* if (tmp->next == NULL)
+		{
+		curr->next = NULL;
+		delete tmp;
+		}*/
+		curr = curr->next;
+
+	}
+}
+
+void deleteEl(stack* begin)
+{
+	int k = 0;
+	stack* i = begin;
+	while (i->next != NULL)
+	{
+		k++;
+		if (k % 2 == 0)
+		{
+			i = i->next;
+		}
+		else
+		{
+			stack* dop = i->next;
+			i->next = dop->next;
+			delete dop;
+		}
+	}
+	cout << "The problem is solved" <<'\n';
+}
+
+void Sort_p(stack** begin)
+{
+	stack* t = NULL, * t1, * temp;
+	if ((*begin)->next->next == NULL) return;
+	do
+	{
+		for (t1 = *begin; t1->next->next != t; t1 = t1->next)
+			if (t1->next->inf > t1->next->next->inf)
+			{
+				temp = t1->next->next;
+				t1->next->next = temp->next;
+				temp->next = t1->next;
+				t1->next = temp;
+			}
+		t = t1->next;
+	} while ((*begin)->next->next != t);
+}
+
+void Sort_inf(stack* begin)
+{
+	stack* t = NULL, * t1;
+	int temp;
+	do {
+		for (t1 = begin; t1->next != t; t1 = t1->next)
+			if (t1->inf > t1->next->inf) {
+				temp = t1->inf;
+				t1->inf = t1->next->inf;
+				t1->next->inf = temp;
+			}
+		t = t1;
+	} while (begin->next != t);
+}
+
+int menu()
+{
+	cout << "1 - add element" << endl;
+	cout << "2 - show stack" << endl;
+	cout << "3 - clear stack" << endl;
+	cout << "4 - stack sort" <<endl;
+	cout <<"0 - exit" << endl;
+	int choise;
+	input(choise);
+	return choise;
+}
+int main()
+{
+	int vibor;
+	stack* begin = NULL;
+	while (true) {
+		switch (menu())
+		{
+		case 1:
+			cout << "Enter the element:" << endl;
+			begin = addElements(begin);
+			cout << "Element added" <<endl;
+			break;
+		case 2:
+			showStack(begin);
+			break;
+		case 3:
+			deleteEl(begin);
+			break;
+		case 4:
+			cout << "Please choose type of sort:" << endl;
+			cout << "1 - Pointer sort" << endl;
+			cout << "2 - Info swap sort" <<endl;
+			cin >> vibor;
+			if (vibor == 1)
+			{
+				Sort_p(&begin);
+			}
+			if (vibor == 2)
+			{
+				Sort_inf(begin);
+			}
+			break;
+		case 0:
+			return 0;
+			break;
+		default:
+			cout << "Choose 1-4 or 0"<< endl;
+			break;
+		}
+	}
+}
---
+++ Lab11/Lab11/Lab11.cpp
@@ -0,0 +1,201 @@
+﻿#include <limits>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+struct Stack {
+	int info;	     
+	Stack* next;     
+} *stackBegin;
+
+
+Stack* Push(Stack* p, int in) {
+	Stack* t = new Stack;			
+	t->info = in;			
+	t->next = p;		
+	return t;
+}
+
+void View(Stack* p) {
+	Stack* t = p;
+	while (t != NULL) {
+		cout << t->info << endl;
+		t = t->next;
+	}
+}
+
+Stack* Pop(Stack* p, int* out) {
+	Stack* t = p;			
+	*out = p->info;
+	p = p->next; 		
+	delete t; 			
+	return p; 			
+}
+
+//Stack* Peek(Stack* p, int* out) {
+//	Stack* t = p;			// Устанавливаем указатель t на вершину p
+//	*out = p->info;
+//	return p; 			// Возвращаем старую вершину p
+//}
+
+void input(int& a)
+{
+	while (true)
+	{
+		cin >> a;
+		if (cin.good()) {
+			break;
+		}
+		cout << "Wrong input" << endl;
+	}
+}
+Stack* addElements(Stack* begin)
+{
+	int inf;
+	input(inf);
+	Stack* t = new Stack;
+	t->info = inf;
+	t->next = begin;
+	return t;
+}
+
+void Sort_pointer(Stack** p) {
+	Stack* t = NULL, * t1, * temp;
+	if ((*p)->next->next == NULL) return;
+	do {
+		for (t1 = *p; t1->next->next != t; t1 = t1->next)
+			if (t1->next->info > t1->next->next->info) {
+				temp = t1->next->next;
+				t1->next->next = temp->next;
+				temp->next = t1->next;
+				t1->next = temp;
+			}
+		t = t1->next;
+	} while ((*p)->next->next != t);
+
+	
+}
+void Sort_inf(Stack* p)
+{
+	Stack* t = NULL, * t1;
+	int temp;
+	do {
+		for (t1 = p; t1->next != t; t1 = t1->next)
+			if (t1->info > t1->next->info) {
+				temp = t1->info;
+				t1->info = t1->next->info;
+				t1->next->info = temp;
+			}
+		t = t1;
+	} while (p->next != t);
+}
+void Del_All(Stack** p) {
+	Stack* t;
+	while (*p != NULL) {
+		t = *p;
+		*p = (*p)->next;
+		delete t;
+	}
+}
+
+void Remove_odd(Stack** p)
+{
+	Stack* current = *p, * lastEven;
+
+
+	while (current != NULL && current->info % 2 != 0)                                
+	{
+		current = current->next;
+	}
+	*p = current;                                                
+
+	if (current == NULL) {                                      
+		return;
+	}
+
+	lastEven = current;                                           
+
+	current = current->next;                                            
+
+	while (current != NULL)                                                  
+	{
+		
+		if (current->info % 2 == 0) {
+
+			lastEven->next = current;                                                                 
+			lastEven = current;                                                            
+		}
+		current = current->next;
+	}
+	lastEven->next = NULL;                                                                  
+}
+
+
+int main()
+{
+	int numberOfElements;
+	cout << "Enter number of elements\n";
+	cin >> numberOfElements;
+
+	cout << "\nEnter numbers:\n";
+	for (int i = 0; i < numberOfElements; i++) {
+		int element;
+		cin >> element;
+		stackBegin = Push(stackBegin, element);
+	}
+	while (true) {
+		cout << "1. Sort by pointer" << endl;
+		cout << "2. Sort by info" << endl;
+		cout << "3. Remove odd numbers" << endl;
+		cout << "4. Add elements" << endl;
+
+		int choice;
+		cin >> choice;
+
+
+
+		switch (choice)
+		{
+
+
+
+
+		case 1:
+		{
+			int el = 100;
+			stackBegin = Push(stackBegin, 100);
+			Sort_pointer(&stackBegin);
+			stackBegin = Pop(stackBegin, &el);
+			View(stackBegin);
+		}
+		break;
+
+
+		case 2:
+		{
+			Sort_inf(stackBegin);
+			View(stackBegin);
+		}
+		break;
+
+		case 3:
+		{
+			
+			Remove_odd(&stackBegin);
+			View(stackBegin);
+		}
+		break;
+		case 4:
+		{
+			cout << "Enter the element:" << endl;
+			stackBegin = addElements(stackBegin);
+			cout << "Element added" << endl;
+			View(stackBegin);
+		}
+		break;
+		}
+	}
+}
+
+
+
